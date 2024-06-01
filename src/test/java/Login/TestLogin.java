3b6a501b81be267900a6362399a811aa8a36dd9c
Prajwal019 <prajwal.pr@aspireapp.com>
2024-06-01 13:40:24 +0530

    Framework

---
+++ src/test/java/Login/TestLogin.java
@@ -0,0 +1,13 @@
+package Login;
+
+import org.testng.annotations.Test;
+
+public class TestLogin
+{
+
+    @Test
+    public void testLoginCred()
+    {
+        System.out.println("Hello");
+    }
+}
